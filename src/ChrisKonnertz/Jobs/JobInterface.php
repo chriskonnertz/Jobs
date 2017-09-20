37eb4a5a1f767f8c2ef840be1ef4ca497b2e3494
Chris Konnertz <siconize@gmail.com>
2017-09-20 14:36:40 +0200

    Improved comments

--- src/ChrisKonnertz/Jobs/JobInterface.php
+++ src/ChrisKonnertz/Jobs/JobInterface.php
@@ -1,13 +1,35 @@
-<?php namespace ChrisKonnertz\Jobs;
+<?php 
+
+namespace ChrisKonnertz\Jobs;
 
 interface JobInterface {
 
+    /**
+     * Returns the name of the job.
+     * 
+     * @return boolean
+     */
     public function getName();
-
+   
+    /**
+     * Returns true if the job is active, false if it is paused (=not executed)
+     * 
+     * @return boolean
+     */
     public function getActive();
-
+   
+    /**
+     * Returns the cool down time
+     * 
+     * @return integer
+     */
     public function getInterval();
 
+    /**
+     * This method is called when the job is run. Overwrite it in your Job class.
+     * 
+     * @return void
+     */
     public function run($executed);   
 
 }
