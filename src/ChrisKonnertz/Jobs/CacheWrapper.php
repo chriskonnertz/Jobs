29f73aa702ab89663429920d51e7dce75c948230
ChrisKonnertz <siconize@gmail.com>
2017-09-20 14:45:55 +0200

    Added inheritDoc tag

--- src/ChrisKonnertz/Jobs/CacheWrapper.php
+++ src/ChrisKonnertz/Jobs/CacheWrapper.php
@@ -7,21 +7,33 @@ use Cache;
 class CacheWrapper implements CacheInterface
 {
 
+    /**
+     * {@inheritDoc}
+     */
     public function has($key)
     {
         return Cache::has($key);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function forever($key, $value)    
     {
         Cache::forever($key, $value);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function get($key)
     {
         return Cache::get($key);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function forget($key)
     {
         Cache::forget($key);
