75142a8ac3fe29cfccc48560835155374ff749c3
ChrisKonnertz <siconize@gmail.com>
2017-09-20 14:44:22 +0200

    Added (phpdoc) comments

--- src/ChrisKonnertz/Jobs/CacheInterface.php
+++ src/ChrisKonnertz/Jobs/CacheInterface.php
@@ -2,12 +2,37 @@
 
 interface CacheInterface {
 
+    /**
+     * Returns true if an item with the given ke exists
+     *
+     * @param string $key
+     * @return bool
+     */
     public function has($key);
 
+    /**
+     * Stores an item with a given key in the cache without expiration
+     *
+     * @param string $key
+     * @param mixed $value
+     * @return void
+     */
     public function forever($key, $value);
 
+    /**
+     * Returns an item of the cache. May return null
+     *
+     * @param string $key
+     * @return mixed
+     */
     public function get($key);
 
+    /**
+     * Removes an item with a given key from the cache
+     *
+     * @param string $key
+     * @return void
+     */
     public function forget($key);
 
 }
